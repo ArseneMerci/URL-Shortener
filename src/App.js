fa37a96349d234e149bf5323019fd27551be1134
ArseneMerci <harsenemerci@gmail.com>
2022-01-10 13:28:06 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+
+import { useState } from "react";
 
 function App() {
+  const [url, setUrl] = useState('');
+  const [shortUrl, setShortUrl] = useState('Your short Url');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.get(`https://api.shrtco.de/v2/shorten?url=${url}`).then((res)=>{
+      setShortUrl(res.data.result.short_link)
+    }).catch((err)=>{
+      alert(err)
+    })
+  }
+  const handleCopy = ()=>{
+    navigator.clipboard.writeText(shortUrl);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+      <input
+       type="text" 
+       className="inputBox"
+       required 
+       value={url}
+       onChange={(e) => setUrl(e.target.value)}
+       />
+      <input type="submit" value="Shorten It" className="submitBox"/>
+      </form>
+<div className="input_copy_wrapper responseBox">
+  <div className="input_copy">
+    <span className="resp" value="hello">{shortUrl}</span>
+    <span className="icon right">
+     <img src="http://clipground.com/images/copy-4.png" 
+          title="Click to Copy"
+          onClick={handleCopy}
+      />
+    </span>
+  </div>
+  
+</div>
     </div>
   );
 }
